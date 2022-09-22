e7a21492d2ec9aa58244af9d79da6dd4aa0f5d33
C Marscheidere <christian-marscheider@web.de>
2022-09-22 09:02:56 +0200

    added documentation

--- script/mainjs.js
+++ script/mainjs.js
@@ -1,13 +1,20 @@
+/**
+ * This functin is used to initialize the webpage
+ * 
+ */
+
 async function init() {
     await includeHTML();
     document.getElementById('headline').innerHTML = 'Herzlich willkommen!';
 }
-
+/**
+ * This function is used to include the header
+ */
 async function includeHTML() {
     let includeElements = document.querySelectorAll('[w3-include-html]');
     for (let i = 0; i < includeElements.length; i++) {
         const element = includeElements[i];
-        file = element.getAttribute("w3-include-html"); // "includes/header.html"
+        file = element.getAttribute("w3-include-html"); 
         let resp = await fetch(file);
         if (resp.ok) {
             element.innerHTML = await resp.text();
