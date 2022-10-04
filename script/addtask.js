64fce18253299f768a7fc9c8b4601f21c22d5de8
Gerrit Pickart <gerrit.pickart@gmail.com>
2022-10-04 15:42:04 +0200

    added addtaskJS

---
+++ script/addtask.js
@@ -0,0 +1,33 @@
+let allTasks = [];
+
+function addTask(){
+    let title = document.getElementById('title').value;
+    let description = document.getElementById('description').value;
+    let category = document.getElementById('category').value;
+    let assigned = document.getElementById('assigned').value;
+    let date = document.getElementById('date').value;
+    /* let prio = document.getElementById('title').value; */
+    /* let subtask = document.getElementById('subtask').value; */
+
+    
+
+let task = {
+    'title': title,
+    'description': description,
+    'category': category,
+    'assigned': assigned,
+    'date': date,
+    /* 'title': title,
+    'title': title, */
+};
+
+allTasks.push(task);
+let allTasksAsString = JSON.stringify(allTasks);
+localStorage.setItem('allTasks', allTasksAsString);
+
+}
+
+function loadAllTasks(){
+   let allTasksAsString = localStorage.getItem('allTasks');
+   allTasks = JSON.parse(allTasksAsString);
+}
