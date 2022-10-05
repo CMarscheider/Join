31ba4f14df38be54def132375a8b3bd391b78fe9
Mazin Fouad <m.fouad@live.de>
2022-10-05 14:47:32 +0200

    $*

--- script/board.js
+++ script/board.js
@@ -1,3 +1,5 @@
+const { arrow } = require('@popperjs/core');
+
 let title = document.getElementById('title');
 let description = document.getElementById('description');
 let category = document.getElementById('category');
