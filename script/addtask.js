0cbf835ed087b16a1ae5f8c8a183b1a230ff7f85
Gerrit Pickart <gerrit.pickart@gmail.com>
2022-10-05 14:30:36 +0200

    $*

--- script/addtask.js
+++ script/addtask.js
@@ -1,31 +1,30 @@
 let allTasks = [];
 let prio;
+loadAllTasks();
 
 function addTask(){
-    let title = document.getElementById('title').value;
-    let description = document.getElementById('description').value;
-    let category = document.getElementById('category').value;
-    let assigned = document.getElementById('assigned').value;
-    let date = document.getElementById('date').value;
-    /* let prio = document.getElementById('title').value; */
+    let title = document.getElementById('title');   
+    let description = document.getElementById('description');
+    let category = document.getElementById('category');
+    let assigned = document.getElementById('assigned');
+    let date = document.getElementById('date');
+    
+   
     /* let subtask = document.getElementById('subtask').value; */
 
-    
-
 let task = {
-    'title': title,
-    'description': description,
-    'category': category,
-    'assigned': assigned,
-    'date': date,
-    /* 'title': title,
-    'title': title, */
+    'title': title.value,
+    'description': description.value,
+    'category': category.value,
+    'assigned': assigned.value,
+    'date': date.value,
+    'prio': prio,
 };
 
 allTasks.push(task);
 let allTasksAsString = JSON.stringify(allTasks);
 localStorage.setItem('allTasks', allTasksAsString);
-console.log(allTasks);
+console.log(task);
 }
 
 function loadAllTasks(){
@@ -37,3 +36,4 @@ function loadAllTasks(){
 function checkPriority(priority){
     prio = priority;
 }
+
