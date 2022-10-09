a5a53c5e80a81e11651bc16185052bfeb949f9dc
Mazin Fouad <m.fouad@live.de>
2022-10-09 20:02:40 +0200

    $*

--- script/board.js
+++ script/board.js
@@ -2,10 +2,10 @@ async function init() {
   await downloadFromServer();
   users = JSON.parse(backend.getItem('users')) || [];
 }
-
+let todoContent;
 function renderTasks() {
   setTimeout(() => {
-    let todoContent = document.getElementById('todoContent');
+    todoContent = document.getElementById('todoContent');
     todoContent.innerHTML = '';
     for (let i = 0; i < allTasks.length; i++) {
       const task = allTasks[i];
@@ -48,4 +48,41 @@ function renderTasks() {
 
 function showInputsForm() {
   document.getElementById('form').classList.remove('d-none');
+}
+
+function searchContent(value) {
+  todoContent.innerHTML = '';
+  for (let i = 0; i < allTasks.length; i++) {
+    let task = allTasks[i];
+    if (task.title.includes(value)) {
+      todoContent = document.getElementById('todoContent');
+      todoContent.innerHTML += /*html*/ `
+      <div class="todo-content">
+              <span class="category">${task.category}</span>
+              <h3>${task.title}</h3>
+              <p>${task.description}</p>
+
+
+              <!-- todo -->
+              <div class="progress-bar-container">
+                <progress id="file" value="32" max="100">32%</progress>
+                <label class="label" for="file">Done1/2</label>
+              </div>
+
+              <div class="box-footer">
+                <div class="footer-circels">
+                  <span>AS</span>
+                  <span>DE</span>
+                  <span>+2</span>
+                </div>
+
+                <div class="footer-images-container">
+                  <img src="./assets/img/arrow-down.png" alt="arrow-up" />
+                  <img src="./assets/img/arrow-down.png" alt="arrow-up" />
+                </div>
+              </div>
+            </div>
+      `;
+    }
+  }
 }
