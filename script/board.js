8b6514cfb7f1e5d1bce3e2cd314aab5b815802f5
Mazin Fouad <m.fouad@live.de>
2022-10-11 11:18:05 +0200

    $*

--- script/board.js
+++ script/board.js
@@ -9,39 +9,7 @@ function renderTasks() {
     todoContent.innerHTML = '';
     for (let i = 0; i < allTasks.length; i++) {
       const task = allTasks[i];
-      console.log(task.title);
-      console.log(task.description);
-      console.log(task.category);
-      console.log(task.date);
-      console.log(task.prio);
-
-      todoContent.innerHTML += /*html*/ `
-      <div class="todo-content">
-              <span class="category">${task.category}</span>
-              <h3>${task.title}</h3>
-              <p>${task.description}</p>
-
-
-              <!-- todo -->
-              <div class="progress-bar-container">
-                <progress id="file" value="32" max="100">32%</progress>
-                <label class="label" for="file">Done1/2</label>
-              </div>
-
-              <div class="box-footer">
-                <div class="footer-circels">
-                  <span>AS</span>
-                  <span>DE</span>
-                  <span>+2</span>
-                </div>
-
-                <div class="footer-images-container">
-                  <img src="./assets/img/arrow-down.png" alt="arrow-up" />
-                  <img src="./assets/img/arrow-down.png" alt="arrow-up" />
-                </div>
-              </div>
-            </div>
-      `;
+      todoContent.innerHTML += taskCardHTML(task);
     }
   }, 300);
 }
@@ -56,33 +24,45 @@ function searchContent(value) {
     let task = allTasks[i];
     if (task.title.includes(value)) {
       todoContent = document.getElementById('todoContent');
-      todoContent.innerHTML += /*html*/ `
-      <div class="todo-content">
-              <span class="category">${task.category}</span>
-              <h3>${task.title}</h3>
-              <p>${task.description}</p>
-
-
-              <!-- todo -->
-              <div class="progress-bar-container">
-                <progress id="file" value="32" max="100">32%</progress>
-                <label class="label" for="file">Done1/2</label>
-              </div>
-
-              <div class="box-footer">
-                <div class="footer-circels">
-                  <span>AS</span>
-                  <span>DE</span>
-                  <span>+2</span>
-                </div>
-
-                <div class="footer-images-container">
-                  <img src="./assets/img/arrow-down.png" alt="arrow-up" />
-                  <img src="./assets/img/arrow-down.png" alt="arrow-up" />
-                </div>
-              </div>
-            </div>
-      `;
+      todoContent.innerHTML += taskCardHTML(task);
     }
   }
+}
+
+function showTaskPopup() {
+  document.getElementById('taskPopup').classList.remove('d-none');
+}
+
+function cancelTaskPopup() {
+  document.getElementById('taskPopup').classList.add('d-none');
+}
+
+function taskCardHTML(task) {
+  return /*html*/ `
+  <div class="todo-content" onclick="showTaskPopup()">
+          <span class="category">${task.category}</span>
+          <h3>${task.title}</h3>
+          <p>${task.description}</p>
+
+
+          <!-- todo -->
+          <div class="progress-bar-container">
+            <progress id="file" value="32" max="100">32%</progress>
+            <label class="label" for="file">Done1/2</label>
+          </div>
+
+          <div class="box-footer">
+            <div class="footer-circels">
+              <span>AS</span>
+              <span>DE</span>
+              <span>+2</span>
+            </div>
+
+            <div class="footer-images-container">
+              <img src="./assets/img/arrow-down.png" alt="arrow-up" />
+              <img src="./assets/img/arrow-down.png" alt="arrow-up" />
+            </div>
+          </div>
+        </div>
+  `;
 }
