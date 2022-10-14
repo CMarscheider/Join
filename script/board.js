d43f2e328e93c4e50f6ab00b98fd593853df25b9
Mazin Fouad <m.fouad@live.de>
2022-10-14 22:46:21 +0200

    $*

--- script/board.js
+++ script/board.js
@@ -5,6 +5,9 @@ async function init() {
 let todoContent;
 let todoFooter;
 
+/**
+ * render all tasks card content
+ */
 function renderTasks() {
   setTimeout(() => {
     todoContent = document.getElementById('todoContent');
@@ -12,22 +15,28 @@ function renderTasks() {
     for (let i = 0; i < allTasks.length; i++) {
       let task = allTasks[i];
       todoContent.innerHTML += taskCardHTML(task, i);
-
-      todoFooter = document.getElementById('boxFooter');
-
-      for (let j = 0; j < task['assigned'].length; j++) {
-        let assigend = task['assigned'][j];
-        let firstLetter = assigend.charAt(0);
-        let secondLetter = assigend.split(' ').pop()[0];
-
-        console.log(firstLetter, secondLetter);
-
-        //Todo: render card footer
-      }
+      renderTaskFooter(task, i);
     }
   }, 500);
 }
 
+/**
+ * reder tasks cards footer content
+ */
+function renderTaskFooter(task, i) {
+  todoFooter = document.getElementById(`boxFooter${i}`);
+  for (let j = 0; j < task['assigned'].length; j++) {
+    let assigend = task['assigned'][j];
+    let firstLetter = assigend.charAt(0);
+    let secondLetter = assigend.split(' ').pop()[0];
+    let restAssigendLength = task['assigned'].splice(1).length;
+    todoFooter.innerHTML += TaskCardFooterHTML(firstLetter, secondLetter, restAssigendLength);
+  }
+}
+
+/**
+ * to show Addtask form
+ */
 function showInputsForm() {
   document.getElementById('form').classList.remove('d-none');
 }
@@ -38,11 +47,14 @@ function searchContent(value) {
     let task = allTasks[i];
     if (task.title.includes(value)) {
       todoContent = document.getElementById('todoContent');
-      todoContent.innerHTML += taskCardHTML(task);
+      todoContent.innerHTML += taskCardHTML(task, i);
     }
   }
 }
 
+/**
+ * click function on the cards to show popup
+ */
 function showTaskPopup(i) {
   document.getElementById('taskPopup').classList.remove('d-none');
   document.getElementById('categoryPopup').innerHTML = allTasks[i].category;
@@ -56,12 +68,12 @@ function cancelTaskPopup() {
 }
 
 function taskCardHTML(task, i) {
+  cat = document.getElementsByClassName(`category${i}`);
   return /*html*/ `
   <div class="todo-content" onclick="showTaskPopup(${i})">
-          <span class="category">${task.category}</span>
+          <span class="category${i}">${task.category}</span>
           <h3>${task.title}</h3>
           <p>${task.description}</p>
-
 
           <!-- todo -->
           <div class="progress-bar-container">
@@ -69,18 +81,30 @@ function taskCardHTML(task, i) {
             <label class="label" for="file">Done1/2</label>
           </div>
 
-          <div class="box-footer" id ="boxFooter">
-            <div class="footer-circels">
-              <span>AS</span>
-              <span>DE</span>
-              <span>+2</span>
-            </div>
+          <div class="box-footer" id ="boxFooter${i}">
+          
 
-            <div class="footer-images-container">
-              <img src="./assets/img/arrow-down.png" alt="arrow-up" />
-              <img src="./assets/img/arrow-down.png" alt="arrow-up" />
-            </div>
+           
           </div>
         </div>
   `;
+}
+
+function TaskCardFooterHTML(firstLetter, secondLetter, restAssigendLength) {
+  return /*html*/ `
+  <div class="footer-circels">
+        <span>${firstLetter}${secondLetter}</span>
+        <span>${restAssigendLength}</span>
+      </div>
+
+      <div class="footer-images-container">
+        <img src="./assets/img/arrow-down.png" alt="arrow-up" />
+        <img src="./assets/img/arrow-down.png" alt="arrow-up" />
+      </div>
+  `;
+}
+
+function generateRandomColor() {
+  let randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+  return randomColor;
 }
