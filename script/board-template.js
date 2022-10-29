f7e882119218f973513107fd882660fb932512d0
C Marscheidere <christian-marscheider@web.de>
2022-10-29 18:37:29 +0200

    $*

--- script/board-template.js
+++ script/board-template.js
@@ -1,6 +1,6 @@
-function openTaskCard(task, i) {
+function TaskCard(taskCategory, task, i) {
   return /*html*/ `
-      <div class="todo-content" onclick="showOpenTaskPopup(${task})" draggable="true" ondragstart="startDragging('open ${i}')">
+      <div class="todo-content" onclick="showOpenTaskPopup(${i})" draggable="true" ondragstart="startDragging(${i})">
               <span class="category${i}" id="category${i}">${task.category.name}</span>
               <h3>${task.title}</h3>
               <p>${task.description}</p>
@@ -11,15 +11,16 @@ function openTaskCard(task, i) {
                 <label class="label" for="file">Done1/2</label>
               </div>
     
-              <div class="box-footer" id ="openTaskFooter${i}">
+              <div class="box-footer" id ="footer${i}">
               
                
               </div>
             </div>
       `;
+      
 }
 
-function openTaskCardFooter(firstLetter, secondLetter, restAssigendLength, j) {
+function footerTemplate(firstLetter, secondLetter, restAssigendLength, j) {
   return /*html*/ `
       <div class="footer-circels">
             <span id="firstLettersContainer">${firstLetter}${secondLetter}</span>
@@ -31,125 +32,4 @@ function openTaskCardFooter(firstLetter, secondLetter, restAssigendLength, j) {
             <img src="./assets/img/arrow-down.png" alt="arrow-up" />
           </div>
       `;
-}
-
-function inProgressTaskCard(inProgressTask, i) {
-  return /*html*/ `
-  <div class="todo-content"  draggable="true" ondragstart="startDragging('categoryProgress ${i}')">
-           <span class="category${i}" id="categoryProgress${i}">inProgress${inProgressTask.category.name}</span>
-           <h3>${inProgressTask.title}</h3>
-           <p>${inProgressTask.description}</p>
- 
-           <!-- todo -->
-           <div class="progress-bar-container">
-             <progress id="file" value="32" max="100">32%</progress>
-             <label class="label" for="file">Done1/2</label>
-           </div>
-           <div class="box-footer" id ="inProgressFooter${i}">      
-           </div>
-         </div>
- `;
-}
-
-function inProgressTaskCardFooter(firstLetter, secondLetter, restAssigendLength, j) {
-  return /*html*/ `
-  <div class="footer-circels">
-          <span id="firstLettersContainer">${firstLetter}${secondLetter}</span>
-          <span id="restLengt">${restAssigendLength}</span>
-        </div>
-  
-        <div class="footer-images-container">
-          <img src="./assets/img/arrow-down.png" alt="arrow-up" />
-          <img src="./assets/img/arrow-down.png" alt="arrow-up" />
-        </div>
-  `;
-}
-
-function awaitingFeedBackTaskCard(awaitingFeedbackTask, i) {
-  return /*html*/ `
-  <div class="todo-content" draggable="true" ondragstart="startDragging('categoryAwaiting ${i}')">
-           <span class="category${i}" id="categoryAwaiting${i}">${awaitingFeedbackTask.category.name}</span>
-           <h3>${awaitingFeedbackTask.title}</h3>
-           <p>${awaitingFeedbackTask.description}</p>
- 
-           <!-- todo -->
-           <div class="progress-bar-container">
-             <progress id="file" value="32" max="100">32%</progress>
-             <label class="label" for="file">Done1/2</label>
-           </div>
-           <div class="box-footer" id ="awaitingFeedbackFooter${i}">      
-           </div>
-         </div>
- `;
-}
-
-function awaitingFeedbackFooter(firstLetter, secondLetter, restAssigendLength, j) {
-  return /*html*/ `
-  <div class="footer-circels">
-          <span id="firstLettersContainer">${firstLetter}${secondLetter}</span>
-          <span id="restLenght">${restAssigendLength}</span>
-        </div>
-  
-        <div class="footer-images-container">
-          <img src="./assets/img/arrow-down.png" alt="arrow-up" />
-          <img src="./assets/img/arrow-down.png" alt="arrow-up" />
-        </div>
-  `;
-}
-
-function doneTaskCard(doneTask, i) {
-  return /*html*/ `
-  <div class="todo-content" draggable="true" ondragstart="startDragging('categoryDone ${i}')">
-           <span class="category${i}" id="categoryDone${i}">${doneTask.category.name}</span>
-           <h3>${doneTask.title}</h3>
-           <p>${doneTask.description}</p>
- 
-           <!-- todo -->
-           <div class="progress-bar-container">
-             <progress id="file" value="32" max="100">32%</progress>
-             <label class="label" for="file">Done1/2</label>
-           </div>
-           <div class="box-footer" id ="doneFooter${i}">      
-           </div>
-         </div>
- `;
-}
-
-function doneFooter(firstLetter, secondLetter, restAssigendLength, j) {
-  return /*html*/ `
-  <div class="footer-circels">
-          <span id="firstLettersContainer">${firstLetter}${secondLetter}</span>
-          <span id="restLength">${restAssigendLength}</span>
-        </div>
-  
-        <div class="footer-images-container">
-          <img src="./assets/img/arrow-down.png" alt="arrow-up" />
-          <img src="./assets/img/arrow-down.png" alt="arrow-up" />
-        </div>
-  `;
-}
-
-function urgentPriority(i) {
-  document.getElementById(
-    'prio'
-  ).innerHTML = `<div class="prio-container-popup"><b>Priority:</b> <span id="prio-status">${allTasks[i].prio} <img id="prio-icon" src="/assets/img/Prio_alta.png"></span></div>`;
-  document.getElementById('prio-status').style.background = 'red';
-}
-
-function mediumPriority(i) {
-  document.getElementById(
-    'prio'
-  ).innerHTML = 65`<div class="prio-container-popup"><b>Priority:</b> <span id="prio-status">${allTasks[i].prio} <img id="prio-icon" src="/assets/img/Prio media.png"></span></div>`;
-  document.getElementById('prio-status').style.background = 'orange';
-}
-
-function lowPriority(i) {
-  document.getElementById(
-    'prio'
-  ).innerHTML = `<div class="prio-container-popup"><b>Priority:</b> <span id="prio-status">${allTasks[i].prio} <img id="prio-icon" src="/assets/img/Prio baja.png"></span></div>`;
-  document.getElementById('prio-status').style.background = 'green';
-}
-
-function cancelTaskPopup() {
-  document.getElementById('taskPopup').classList.add('d-none');
 }
--- script/board.js
+++ script/board.js
@@ -3,131 +3,70 @@ async function init() {
   users = JSON.parse(backend.getItem('users')) || [];
 }
 let currentDraggedElement;
-let task;
-let inProgressTask;
-let awaitingFeedbackTask;
-let doneTask;
-let openTasks;
-let inProgressTasks;
-let awaitingFeedback;
-let doneTasks;
 let currentCategory;
+let taskCategory;
 /**
  * call all render functions for tasks uin board
  */
 function renderTasks() {
   setTimeout(() => {
-    renderOpenTasks();
-    renderInProgressTasks();
-    renderInProgressTasks();
-    renderAwaitingFeedbackTasks();
-    renderDoneTasks();
+    startRendering();
   }, 300);
 }
 
-function renderOpenTasks() {
+function resetAllTasks(openTasksContent, inProgressTasksContent, awaitingFeedbackContent, doneTasksContent){
+  openTasksContent.innerHTML="";
+  inProgressTasksContent.innerHTML="";
+  awaitingFeedbackContent.innerHTML="";
+  doneTasksContent.innerHTML="";
+}
+
+function startRendering() {
   let openTasksContent = document.getElementById('todoOpenContent');
-  openTasksContent.innerHTML = '';
-  openTasks = allTasks.filter((t) => t.status === 'open');
-  /* openTasksContent.innerHTML = ''; */
-  for (let i = 0; i < openTasks.length; i++) {
-    task = openTasks[i];
-    openTasksContent.innerHTML += openTaskCard(task, i);
-    renderOpenTaskFooter(task, i);
-    styleCategory(task, i);
-  }
-}
-
-function renderOpenTaskFooter(task, j) {
-  let todoFooter = document.getElementById(`openTaskFooter${j}`);
-  for (let j = 0; j < task['assigned'].length; j++) {
-    let assigend = task['assigned'][j];
-
-    let firstLetter = assigend.charAt(0);
-    let secondLetter = assigend.split(' ')[1].charAt(0);
-    let restAssigendLength = task['assigned'].splice(1).length;
-    todoFooter.innerHTML += openTaskCardFooter(firstLetter, secondLetter, restAssigendLength, j);
-
-    if (task.assigned.length == 0) {
-      document.getElementById(`restLength${j}A`).classList.add('d-none');
-    }
-  }
-}
-
-function renderInProgressTasks() {
   let inProgressTasksContent = document.getElementById('todoInProgressContent');
-  inProgressTasksContent.innerHTML = '';
-  inProgressTasks = allTasks.filter((t) => t.status === 'inProgress');
-  inProgressTasksContent.innerHTML = '';
-  for (let i = 0; i < inProgressTasks.length; i++) {
-    inProgressTask = inProgressTasks[i];
-    inProgressTasksContent.innerHTML += inProgressTaskCard(inProgressTask, i);
-    renderInProgressTaskFooter(inProgressTask, i);
-    styleCategoryProgress(inProgressTask, i);
-  }
-}
-
-function renderInProgressTaskFooter(task, j) {
-  let todoInProgressFooter = document.getElementById(`inProgressFooter${j}`);
-  for (let j = 0; j < task['assigned'].length; j++) {
-    let assigend = task['assigned'][j];
-    let firstLetter = assigend.charAt(0);
-    let secondLetter = assigend.split(' ')[1].charAt(0);
-    let restAssigendLength = task['assigned'].splice(1).length;
-    todoInProgressFooter.innerHTML += inProgressTaskCardFooter(firstLetter, secondLetter, restAssigendLength, j);
-  }
-}
-
-function renderAwaitingFeedbackTasks() {
   let awaitingFeedbackContent = document.getElementById('todoAwaitingFeedbackContent');
-  awaitingFeedbackContent.innerHTML = '';
-  awaitingFeedback = allTasks.filter((t) => t.status === 'awaitingFeedback');
-  for (let i = 0; i < awaitingFeedback.length; i++) {
-    awaitingFeedbackTask = awaitingFeedback[i];
-    awaitingFeedbackContent.innerHTML += awaitingFeedBackTaskCard(awaitingFeedbackTask, i);
-    renderAwaitingFeedbackTaskFooter(i);
-    styleCategoryAwaiting(awaitingFeedbackTask, i);
-  }
-}
-
-function renderAwaitingFeedbackTaskFooter(j) {
-  let todoAwaitingFeedbackFooter = document.getElementById(`awaitingFeedbackFooter${j}`);
-  for (let j = 0; j < task['assigned'].length; j++) {
-    let assigend = task['assigned'][j];
-    let firstLetter = assigend.charAt(0);
-    let secondLetter = assigend.split(' ')[1].charAt(0);
-    let restAssigendLength = task['assigned'].splice(1).length;
-    todoAwaitingFeedbackFooter.innerHTML += awaitingFeedbackFooter(firstLetter, secondLetter, restAssigendLength, j);
-  }
-}
-
-function renderDoneTasks() {
   let doneTasksContent = document.getElementById('todoDoneContent');
-  doneTasksContent.innerHTML = '';
-  doneTasks = allTasks.filter((t) => t.status === 'done');
-  for (let i = 0; i < doneTasks.length; i++) {
-    doneTask = doneTasks[i];
-    doneTasksContent.innerHTML += doneTaskCard(doneTask, i);
-    styleCategoryDone(doneTask, i);
-    renderDoneTasksFooter(i);
+resetAllTasks(openTasksContent, inProgressTasksContent, awaitingFeedbackContent, doneTasksContent);
+console.log(inProgressTasksContent);
+  for (let i = 0; i < allTasks.length; i++) {
+    const printTask = allTasks[i];
+    if (printTask['status'] == "open") {
+      taskCategory = "open";
+      openTasksContent.innerHTML +=  TaskCard(taskCategory, printTask, i);
+    } else if (printTask['status'] == "inProgress") {
+      taskCategory = "inProgress";
+      inProgressTasksContent.innerHTML += TaskCard(taskCategory, printTask, i);
+    } else if (printTask['status'] == "awaitingFeedback") {
+      taskCategory = "awaitingFeedback";
+      awaitingFeedbackContent.innerHTML += TaskCard(taskCategory, printTask, i);
+    } else {
+      taskCategory = "done";
+      doneTasksContent.innerHTML += TaskCard(taskCategory, printTask, i);
+    }
+    
+      renderFooter(taskCategory, i, printTask);
+      styleCategory(printTask, i);
   }
+
 }
 
-function renderDoneTasksFooter(j) {
-  let todoDoneFooter = document.getElementById(`doneFooter${j}`);
-  for (let j = 0; j < task['assigned'].length; j++) {
-    let assigend = task['assigned'][j];
+function renderFooter(taskCategory, i, printTask) {
+  let footer = document.getElementById(`footer${i}`);
+  console.log(footer);
+
+  for (let j = 0; j < printTask['assigned'].length; j++) {
+    let assigend = printTask['assigned'][j];
     let firstLetter = assigend.charAt(0);
     let secondLetter = assigend.split(' ')[1].charAt(0);
-    let restAssigendLength = task['assigned'].splice(1).length;
-    todoDoneFooter.innerHTML += doneFooter(firstLetter, secondLetter, restAssigendLength, j);
+    let restAssigendLength = printTask['assigned'].splice(1).length;
+    footer.innerHTML += footerTemplate(firstLetter, secondLetter, restAssigendLength, j);
   }
 }
 
-function styleCategory(task, k) {
-  let cardCat = document.getElementById(`category${k}`);
-  const cat = cardCat;
-  cat.style.backgroundColor = task.category.color;
+function styleCategory(printTask, b) {
+  let cat = document.getElementById(`category${b}`);
+
+  cat.style.backgroundColor = printTask.category.color;
   cat.style.color = '#fff';
   cat.style.width = '90px';
   cat.style.textAlign = 'center';
@@ -136,41 +75,6 @@ function styleCategory(task, k) {
   cat.style.whiteSpace = 'nowrap';
 }
 
-function styleCategoryProgress(inProgressTask, z) {
-  let cardCat = document.getElementById(`categoryProgress${z}`);
-  const cat = cardCat;
-  cardCat.style.backgroundColor = inProgressTask.category.color;
-  cat.style.color = '#fff';
-  cat.style.width = '90px';
-  cat.style.textAlign = 'center';
-  cat.style.padding = '5px';
-  cat.style.borderRadius = '8px';
-  cat.style.whiteSpace = 'nowrap';
-}
-
-function styleCategoryAwaiting(awaitingFeedbackTask, a) {
-  let cardCat = document.getElementById(`categoryAwaiting${a}`);
-  const cat = cardCat;
-  cardCat.style.backgroundColor = awaitingFeedbackTask.category.color;
-  cat.style.color = '#fff';
-  cat.style.width = '90px';
-  cat.style.textAlign = 'center';
-  cat.style.padding = '5px';
-  cat.style.borderRadius = '8px';
-  cat.style.whiteSpace = 'nowrap';
-}
-
-function styleCategoryDone(doneTask, b) {
-  let cardCat = document.getElementById(`categoryDone${b}`);
-  const cat = cardCat;
-  cardCat.style.backgroundColor = doneTask.category.color;
-  cat.style.color = '#fff';
-  cat.style.width = '90px';
-  cat.style.textAlign = 'center';
-  cat.style.padding = '5px';
-  cat.style.borderRadius = '8px';
-  cat.style.whiteSpace = 'nowrap';
-}
 
 function showInputsForm() {
   document.getElementById('form').classList.remove('d-none');
@@ -194,58 +98,44 @@ function searchContent(value) {
   }
 }
 
-function showOpenTaskPopup(task) {
+function showOpenTaskPopup(i) {
   document.getElementById('taskPopup').classList.remove('d-none');
-  console.log(task);
-  document.getElementById('categoryPopup').innerHTML = task.title;
-  document.getElementById('categoryPopup').style.background = task.category.color;
-  // document.getElementById('titlePopup').innerHTML = allTasks[i].title;
-  // document.getElementById('descriptionPopup').innerHTML = allTasks[i].description;
-  // document.getElementById('datePopup').innerHTML = `<b>Due Date:</b> ${allTasks[i].date}`;
-  // if (task.prio === 'urgent') {
-  //   urgentPriority(i);
-  // }
-  // if (task.prio === 'medium') {
-  //   mediumPriority(i);
-  // }
-  // if (task.prio === 'low') {
-  //   lowPriority(i);
-  // }
-  // document.getElementById('assigendTo').innerHTML = allTasks[i].assigned;
+  document.getElementById('categoryPopup').innerHTML = allTasks[i].title;
+  document.getElementById('categoryPopup').style.background = allTasks[i].category.color;
+  document.getElementById('titlePopup').innerHTML = allTasks[i].title;
+  document.getElementById('descriptionPopup').innerHTML = allTasks[i].description;
+  document.getElementById('datePopup').innerHTML = `<b>Due Date:</b> ${allTasks[i].date}`;
+
+  document.getElementById('prio').innerHTML = `<div class="prio-container-popup"><b>Priority:</b> <span id="prio-status">${allTasks[i].prio} <img id="prio-icon" src="/assets/img/Prio_alta.png"></span></div>`;
+  if (allTasks[i].prio === 'urgent') {
+    document.getElementById('prio-status').style.background = 'red';
+  }
+  if (allTasks[i].prio === 'medium') {
+    document.getElementById('prio-status').style.background = 'orange';
+  }
+  if (allTasks[i].prio === 'low') {
+    document.getElementById('prio-status').style.background = 'green';
+  }
+  document.getElementById('assigendTo').innerHTML = allTasks[i].assigned;
 }
+
+function cancelTaskPopup() {
+  document.getElementById('taskPopup').classList.add('d-none');
+}
+
 
 //Drag and Drop
 function startDragging(id) {
   currentDraggedElement = id;
-  console.log(currentDraggedElement);
-  currentCategory = currentDraggedElement['status'];
-
+  currentCategory = allTasks[currentDraggedElement]['status'];
 }
 
 function allowDrop(ev) {
   ev.preventDefault();
 }
 
-function moveTo(category) {
-  console.log(task);
-console.log(category);
-allTasks[currentDraggedElement]['status'] = category;
-
-/*   if (category == "open") {
-    openTasks[currentDraggedElement]['status'] = category;
-  } else if(category == "inProgress"){
-    inProgressTasks[currentDraggedElement]['status'] = category;
-  } else if (category == "awaitingFeedback"){
-    awaitingFeedback[currentDraggedElement]['status'] = category;
-  } else {
-    doneTasks[currentDraggedElement]['status'] = category;
-  } */
- 
-/*   renderOpenTasks();
-  renderInProgressTasks();
-  renderAwaitingFeedbackTasks();
-  renderDoneTasks(); */
-  
+async function moveTo(category) {
+  allTasks[currentDraggedElement]['status'] = category;
+  await backend.setItem('allTasks', JSON.stringify(allTasks));
   renderTasks();
-
 }
